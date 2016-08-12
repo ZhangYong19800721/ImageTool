f77d6f8260937ce6da28c88f477aec93a76ff45c
ZhangYong19800721 <24452861@qq.com>
2016-08-12 23:25:20 +0800

    update the clip_yuv.m

--- +itool/@YUV/YUV.m
+++ +itool/@YUV/YUV.m
@@ -11,7 +11,26 @@ classdef YUV
         format;
     end
     
-    methods
+    methods(Static)
+        function exit = play(filename,row,col,frame_num,format)
+            fid = fopen(filename,'r');
+            [cr, cc] = itool.color_space(row,col,format);
+            
+            for f = 1:frame_num
+                f
+                Y = fread(fid,[col row],'uchar')';
+                U = fread(fid,[cc cr],'uchar')';
+                V = fread(fid,[cc cr],'uchar')';
+            
+                U = imresize(U,[row col]);
+                V = imresize(V,[row col]);
+                warning off;
+                imshow(ycbcr2rgb(uint8(cat(3,Y,U,V))));
+            end
+            
+            fclose(fid);
+            exit = 0;
+        end
     end
     
 end
---
+++ +itool/clip_yuv.m
@@ -0,0 +1,47 @@
+function flag = clip_yuv( infile_name, frame_start, frame_count, width, height, chroma, outfile_name)
+%CLIPYUV 剪辑一个YUV文件
+%   start_frame 起始帧号
+%   frame_count 剪辑帧数
+
+    file_out = fopen(outfile_name,'w'); % 打开输出文件
+    file_in = fopen(infile_name,'r'); % 打开输入文件
+    
+    if chroma == 'yuv444p'
+        w = width;
+        h = height;
+    elseif chroma == 'yuv420p'
+        w = width/2;
+        h = height/2;
+    elseif chroma == 'yuv422p'
+        w = width/2;
+        h = height;
+    else
+        error('Wrong chroma format');
+    end
+    
+    Y = zeros(width,height);
+    U = zeros(w,h);
+    V = zeros(w,h);
+    
+    
+    for n = 1:(frame_start-1)    
+        Y = fread(file_in,[width height],'uchar');
+        U = fread(file_in,[w h],'uchar');
+        V = fread(file_in,[w h],'uchar');
+    end
+    
+    for frame = 1:frame_count
+        Y = fread(file_in,[width height],'uchar');
+        U = fread(file_in,[w h],'uchar');
+        V = fread(file_in,[w h],'uchar');
+        
+        fwrite(file_out,uint8(Y));
+        fwrite(file_out,uint8(U));
+        fwrite(file_out,uint8(V));
+    end
+    
+    fclose(file_in);
+    fclose(file_out);
+    flag = true;
+end
+
