154944bcd87b3d5b8a211f1badf26b3c00309106
ZhangYong19800721 <24452861@qq.com>
2016-09-14 17:08:49 +0800

    add the gain compensation

--- +itool/@ImageStitcher/stitch.m
+++ +itool/@ImageStitcher/stitch.m
@@ -1,4 +1,4 @@
-function canvas = stitch(obj, images, isline)
+function canvas = stitch(obj, images, options)
 % stitch : 对多个图片进行拼接
 % images : 输入的N个图片
 % canvas ：拼接完成的结果
@@ -8,22 +8,44 @@ function canvas = stitch(obj, images, isline)
     Y = zeros(obj.canvas_row_num,obj.canvas_col_num);
     U = zeros(obj.canvas_row_num,obj.canvas_col_num);
     V = zeros(obj.canvas_row_num,obj.canvas_col_num);
-   
+    M = logical(zeros(size(Y)));
+    
     for n = number_of_images:-1:1
-        image = images(n).image; % 取得图像
-        image_yuv = rgb2ycbcr(image); 
-        image_yuv(:,:,1) = uint8(obj.cameras(n).gain .* double(image_yuv(:,:,1))); % 作亮度调整
-        image = ycbcr2rgb(image_yuv); 
+        image = double(images(n).image);
+        
+        if options.is_gain_compensation
+            image(:,:,1) = obj.cameras(n).gain(1) .* image(:,:,1);
+            image(:,:,2) = obj.cameras(n).gain(2) .* image(:,:,2);
+            image(:,:,3) = obj.cameras(n).gain(3) .* image(:,:,3);
+        end
         
         [image_row_num, image_col_num, ~] = size(image);
         [IX, IY] = meshgrid(1:image_row_num,1:image_col_num);
-        image_mask = logical(obj.cameras(n).mask);
-        Y(image_mask) = interp2(IX,IY,double(image(:,:,1))',obj.cameras(n).query_x,obj.cameras(n).query_y);
-        U(image_mask) = interp2(IX,IY,double(image(:,:,2))',obj.cameras(n).query_x,obj.cameras(n).query_y);
-        V(image_mask) = interp2(IX,IY,double(image(:,:,3))',obj.cameras(n).query_x,obj.cameras(n).query_y);
+        mask2d = reshape(logical(obj.cameras(n).mask),obj.canvas_row_num,obj.canvas_col_num);
         
-        if strcmp(isline,'show_line')
-            color = rgb2ycbcr([255 0 0]);
+        if options.is_blending    
+            Y_I = zeros(obj.canvas_row_num,obj.canvas_col_num); 
+            U_I = zeros(obj.canvas_row_num,obj.canvas_col_num);
+            V_I = zeros(obj.canvas_row_num,obj.canvas_col_num);
+            
+            Y_I(mask2d) = interp2(IX,IY,double(image(:,:,1))',obj.cameras(n).query_x,obj.cameras(n).query_y);
+            U_I(mask2d) = interp2(IX,IY,double(image(:,:,2))',obj.cameras(n).query_x,obj.cameras(n).query_y);
+            V_I(mask2d) = interp2(IX,IY,double(image(:,:,3))',obj.cameras(n).query_x,obj.cameras(n).query_y);
+            
+            Y = itool.MultiBandBlending.test_blend(Y,Y_I,M,mask2d,6);
+            U = itool.MultiBandBlending.test_blend(U,U_I,M,mask2d,6);
+            V = itool.MultiBandBlending.test_blend(V,V_I,M,mask2d,6);
+            M(mask2d) = 1;
+        else
+            Y(mask2d) = interp2(IX,IY,double(image(:,:,1))',obj.cameras(n).query_x,obj.cameras(n).query_y);
+            U(mask2d) = interp2(IX,IY,double(image(:,:,2))',obj.cameras(n).query_x,obj.cameras(n).query_y);
+            V(mask2d) = interp2(IX,IY,double(image(:,:,3))',obj.cameras(n).query_x,obj.cameras(n).query_y);
+        end
+    end
+    
+    if options.is_show_skeleton
+        color = rgb2ycbcr([255 0 0]);
+        for n = number_of_images:-1:1    
             Y(obj.cameras(n).front) = color(1); 
             U(obj.cameras(n).front) = color(2); 
             V(obj.cameras(n).front) = color(3);
