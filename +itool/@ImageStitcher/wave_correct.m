0db459fe500a267ad2cb3336665d11721045ecd7
ZhangYong19800721 <24452861@qq.com>
2016-09-16 09:22:11 +0800

    add wave correction

--- +itool/@ImageStitcher/wave_correct.m
+++ +itool/@ImageStitcher/wave_correct.m
@@ -1,7 +1,12 @@
 function obj = wave_correct(obj)
 %WAVE_CORRECT 波浪修正
 %   此处显示详细说明
-    
-
+    unit_y = [0 1 0]'; % 水平方向的单位向量
+    num = length(obj.cameras);
+    A = zeros(3,3);
+    for n = 1:num
+        Y = obj.cameras(n).K * obj.cameras(n).R * unit_y;
+        A = A + Y*Y';
+    end
 end
 
